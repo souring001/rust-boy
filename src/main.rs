f752a00fa0cac44cdd8b0743fbfa79f7a52c02b1
aso <titechei@gmail.com>
2024-02-03 22:00:07 +0900

    【✅実績解除】双方向一要素リストの実装

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use std::fmt;
 
-struct Node<T> {
-    hoge:T
+#[derive(Clone)]
+struct Node<T: Clone> {
+    data: T,
+    next: Option<Box<Node<T>>>,
+    prev: Option<Box<Node<T>>>,
 }
 
-struct DoublyLinkedList<T> {
-    hoge:T
+struct DoublyLinkedList<T: Clone> {
+    head: Option<Box<Node<T>>>,
+    tail: Option<Box<Node<T>>>,
 }
 
-impl<T> DoublyLinkedList<T> {
+impl<T: Clone> DoublyLinkedList<T> {
     fn new() -> Self {
-        unimplemented!("hey girls!")
+        DoublyLinkedList {
+            head: None,
+            tail: None,
+        }
     }
 
     /// Removes the last element from a list and returns it, or None if it is empty.
@@ -27,43 +34,59 @@ impl<T> DoublyLinkedList<T> {
 
     /// Appends an element to the back of a list.
     /// This operation should compute in O(1) time.
-    fn push_back(&mut self, elt: T) {}
+    fn push_back(&mut self, elt: T) {
+        let new_node = Node {
+            data: elt,
+            next: None,
+            prev: None,
+        };
+        self.head = Some(Box::new(new_node.clone()));
+        self.tail = Some(Box::new(new_node));
+    }
 
     /// Adds an element first in the list.
     /// This operation should compute in O(1) time.
-    fn push_front(&mut self, elt: T) {}
+    fn push_front(&mut self, elt: T) {
+        unimplemented!()
+    }
 }
 
-impl<T> fmt::Display for DoublyLinkedList<T>
+impl<T: Clone> fmt::Display for DoublyLinkedList<T>
 where
     T: fmt::Display,
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        unimplemented!()
-        // let mut current = self.head.clone();
-        // while let Some(node) = current {
-        //     let n = node.borrow();
-        //     write!(f, "{}", n.data)?;
-        //     current = n.next.clone();
-        //     if current.is_some() {
-        //         write!(f, "<--->")?;
-        //     }
-        // }
-        // Ok(())
+        let mut current = self.head.clone();
+        write!(f, "(");
+        while let Some(node) = current {
+            let n = node;
+            write!(f, "{}", n.data)?;
+            current = n.next.clone();
+            if current.is_some() {
+                write!(f, "<--->")?;
+            }
+        }
+        write!(f, ")");
+        Ok(())
     }
 }
 
 fn main() {
     let mut list = DoublyLinkedList::new();
 
+    println!("{}", list); // ()
+
     list.push_back(1);
+
+    println!("{}", list); // (1)
+
     list.push_back(2);
     list.push_back(3);
 
-    println!("{}", list); // 1<--->2<--->3
+    println!("{}", list); // (1<--->2<--->3)
 
     list.push_front(4);
     list.push_front(5);
 
-    println!("{}", list); // 5<--->4<--->1<--->2<--->3
+    println!("{}", list); // (5<--->4<--->1<--->2<--->3)
 }
